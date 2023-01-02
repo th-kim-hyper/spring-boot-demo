11a2f1ba97dfecd69a530f83d6950ae66e3632e0
itsocool <yesican_kor@hotmail.com>
2023-01-02 22:14:00 +0900

    Initial commit

---
+++ src/main/java/kr/co/hyperinfo/demo/TomcatHttpAndHttpsConfig.java
@@ -0,0 +1,26 @@
+package com.memorynotfound.spring.boot.tomcat;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatHttpAndHttpsConfig {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer(){
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector(){
+        Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        return connector;
+    }
+}
---
+++ src/main/java/kr/co/hyperinfo/demo/TomcatRedirectHttpToHttpsConfig.java
@@ -0,0 +1,39 @@
+package com.memorynotfound.spring.boot.tomcat;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Configuration;
+import javax.servlet.annotation.ServletSecurity.TransportGuarantee;
+
+@Configuration
+public class TomcatRedirectHttpToHttpsConfig {
+
+    public EmbeddedServletContainerFactory servletContainer(){
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint(TransportGuarantee.CONFIDENTIAL.name());
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector(){
+        Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
